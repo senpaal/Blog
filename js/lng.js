2bb1cbc85fcf268833367e3556b16d75921bec2b
senpaal <wery.nikita@gmail.com>
2023-04-05 21:13:13 +0300

    update translate

--- js/lng.js
+++ js/lng.js
@@ -1,45 +1,49 @@
-const lngArr = {
+const langArr = {
+    "unit" : {
+        "ru" : "Портфолио",
+        "en" : "Portfolio",
+    },
     "hello" : {
         "ru" : "Привет!",
         "en" : "Hello!",
     },
     "welcome" : {
         "ru" : "Добро пожаловать в мой блог!",
-        "en" : "Welcome to my blog!",
+        "en" : "Welcome to my log!",
     },
     "see" : {
         "ru" : "Что тут можно увидеть?",
         "en" : "What can you see here?",
     },
-    "grid-item-1" : {
+    "grid-1" : {
         "ru" : "Мои учения",
         "en" : "My teachings",
     },
-    "info1" : {
+    "info-1" : {
         "ru" : "Я читал учебник и делал упражнения, чтобы добиться такого",
         "en" : "I read a textbook and did exercises to achieve this",
     },
-    "grid-item-2" : {
+    "grid-2" : {
         "ru" : "Мои развития",
         "en" : "My Developments",
     },
-    "info2" : {
+    "info-2" : {
         "ru" : "Я обретал всё больше и больше знаний, учась на ошибках",
         "en" : "I gained more and more knowledge by learning from mistakes",
     },
-    "grid-item-3" : {
+    "grid-3" : {
         "ru" : "Мои попытки",
         "en" : "My attempts",
     },
-    "info3" : {
+    "info-3" : {
         "ru" : "Не всё было гладко, но я старался и не терял мотивацию",
         "en" : "Not everything was smooth, but I tried and did not lose motivation",
     },
-    "grid-item-4" : {
+    "grid-4" : {
         "ru" : "Мои успехи",
         "en" : "My progress",
     },
-    "info4" : {
+    "info-4" : {
         "ru" : "С помощью моих знаний и умений, я смог сделать этот сайт, и это не всё",
         "en" : "With the help of my knowledge and skills, I was able to make this site, and that's not all",
     },
@@ -54,5 +58,65 @@ const lngArr = {
     "buttonPortfolio" : {
         "ru" : "Портфолио",
         "en" : "Portfolio"
+    },
+    "name" : {
+        "ru" : "Никита",
+        "en" : "Nikita",
+    },
+    "text-info" : {
+        "ru" : `Я <a href="https://vk.com/senpaall" target="_blank">senpaal</a>, фронтендер с небольшим опытом, и страстью к кодированию. <br> Если меня попросят сделать то, чего я не умею, то я научусь и сделаю это. Я спокойный <br> и общительный человек, и могу вписаться в любую компанию. У меня маниакальная любовь <br> к программированию и к его изучению. Итак, если у вас задача где надо подумать, <br> вы нашли нужного человека!`,
+        "en" : `I am <a href="https://vk.com/senpaall" target="_blank">senpaal</a>, a frontender with little experience, and a passion for coding. <br> If I'm asked to do something I don't know how to do, then I'll learn and do it. I am a calm <br> and sociable person, and I can fit into any company. I have a manic love for programming <br> and for studying it. So, if you have a task where you need to think, <br> you have found the right person!`,
+    },
+    "skills" : {
+        "ru" : "Мои возможности",
+        "en" : "My capabilities",
+    },
+    "backend" : {
+        "ru" : "Бэкенд программирование",
+        "en" : "Backend programming",
+    },
+    "BEtext1" : {
+        "ru" : "*В изучении*",
+        "en" : "*In the study*",
+    },
+    "frontend" : {
+        "ru" : "Фронтенд программирование",
+        "en" : "Frontend programming",
+    },
+    "FEtext1" : {
+        "ru" : "Я создаю код с нуля и элегантно <br> воплощаю идеи в жизнь, уделяя <br> максимальное внимание вашему <br> видению продукта.",
+        "en" : "I create code from scratch and <br> elegantly bring ideas to life, paying <br> maximum attention to your <br> vision of the product.",
+    },
+    "FEtext2" : {
+        "ru" : "Используемые языки:",
+        "en" : "Languages used:",
+    },
+    "FEtext3" : {
+        "ru" : "Мои инструменты:",
+        "en" : "My Tools:",
+    },
+    "endText" : {
+        "ru" : "С моими работами вы можете ознакомиться, нажав на кнопку ниже!",
+        "en" : "You can get acquainted with my works by clicking on the button below!",
+    },
+    "buttonMyworks" : {
+        "ru" : "Мои работы",
+        "en" : "My works",
+    },
+    "textMyworks" : {
+        "ru" : "Мои работы",
+        "en" : "My works",
+    },
+    "site__headline" : {
+        "ru" : "Блог",
+        "en" : "Blog",
+    },
+    "infoBlog" : {
+        "ru" : "Веб-сайт блог на ПК. Прост в использовании, присутствуют настройки фона. Есть ссылки на книги для языков программирования.",
+        "en" : "Website blog on PC. Easy to use, there are background settings. There are links to books for programming languages.",
+    },
+    "linksite" : {
+        "ru" : "Посетить сайт",
+        "en" : "View project",
     },
 }
